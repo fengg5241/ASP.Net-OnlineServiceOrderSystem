3061e5979c96b35992561f2a446c8ac7094cbde6
fengg5241 <fengg5241@users.noreply.github.com>
2018-01-14 23:23:42 +0800

    Add files via upload

---
+++ App_Code/ViewinMaps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+/// <summary>
+/// Summary description for ViewinMaps
+/// //I'm so good.
+/// </summary>
+public class ViewinMaps
+{
+    public static string loadMap (string Zipcode)
+    {
+        StringBuilder map = new StringBuilder();
+        map.Append("https://www.google.com.sg/maps/place/Singapore+");
+        map.Append(Zipcode);
+        return map.ToString();
+    }
+
+}
